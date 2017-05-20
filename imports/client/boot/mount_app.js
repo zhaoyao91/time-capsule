a38de16db65fd40d6227dc2a43fae2187d2bfbd5
zhaoyao91 <zhaoyao91@163.com>
2017-05-20 10:09:33 +0800

    mount react app

---
+++ imports/client/boot/mount_app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from '../ui/App'
+
+export default function () {
+  ReactDOM.render(<App/>, document.getElementById('react-root'))
+}

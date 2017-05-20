c24614bb3fc70901aa18084e3a49ebee3194208a
zhaoyao91 <zhaoyao91@163.com>
2017-05-20 11:58:35 +0800

    style home page

--- imports/client/ui/pages/HomePage.js
+++ imports/client/ui/pages/HomePage.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Button } from 'reactstrap'
+import { compose, withProps } from 'recompose'
+import { css }from 'glamor'
 
-export default function HomePage () {
-  return <div>
-    home page
+export default compose(
+  withProps({
+    styles: {
+      page: css({
+        'width': '100vw',
+        'height': '100vh',
+        'display': 'flex'
+      }),
+      contentBox: css({
+        'margin': 'auto'
+      }),
+      headerBox: css({
+        'marginBottom': '1rem',
+        '& > *': {
+          'margin': '0'
+        }
+      }),
+      buttonsBox: css({
+        'display': 'flex',
+        'justifyContent': 'space-between'
+      })
+    }
+  })
+)(function HomePage ({styles}) {
+  return <div {...styles.page}>
+    <div {...styles.contentBox}>
+      <div {...styles.headerBox}>
+        <h1>时光胶囊</h1>
+      </div>
+      <div {...styles.buttonsBox}>
+        <Button color="primary">创建</Button>
+        <Button color="primary">打开</Button>
+      </div>
+    </div>
   </div>
-}
+})

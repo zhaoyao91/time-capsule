70895817e94d171cf0b39ccd836280163323f03d
zhaoyao91 <zhaoyao91@163.com>
2017-05-21 23:55:58 +0800

    finish all views

--- imports/client/ui/pages/TimeCapsulePage.js
+++ imports/client/ui/pages/TimeCapsulePage.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { compose, withProps, withState, withHandlers, lifecycle } from 'recompose'
 import { withRouter } from 'react-router-dom'
 import { css }from 'glamor'
-import { Button } from 'reactstrap'
+import { Alert, Card, CardBlock, CardText } from 'reactstrap'
+import moment from 'moment'
 
 import LinkText from '../views/LinkText'
 import CenterLoading from '../views/CenterLoading'
@@ -57,12 +58,12 @@ export default compose(
 )(function TimeCapsulePage ({state, timeCapsule, reload}) {
     return <MainPageLayout>
       {do {
-        if (state === 'error') <ErrorView reload={reload}/>
+        if (state === 'error') <ErrorView/>
         else if (state === 'loading') <LoadingView/>
         else if (state === 'notFound') <NotFoundView/>
         else if (state === 'notOpen') <NotOpenView reload={reload} timeCapsule={timeCapsule}/>
         else if (state === 'open') <OpenView timeCapsule={timeCapsule}/>
-        else <WrontStateView/>
+        else <WrongStateView/>
       }}
     </MainPageLayout>
   }
@@ -72,26 +73,51 @@ function LoadingView () {
   return <CenterLoading/>
 }
 
-function ErrorView ({reload}) {
-  return <p>
-    <span>打开失败，请</span>
-    <LinkText onClick={reload}>重新尝试</LinkText>
-    <span>。</span>
-  </p>
+function ErrorView () {
+  return <Alert color="danger">打开失败。</Alert>
 }
 
 function NotFoundView () {
-  return <div>not found</div>
+  return <Alert color="info">没有找到这个胶囊。</Alert>
 }
 
-function NotOpenView () {
-  return <div>not open</div>
+function NotOpenView ({reload, timeCapsule}) {
+  return <div>
+    <Alert color="warning">尚未到达开启时间，无法查看胶囊内容。</Alert>
+    <TimeCapsuleCard timeCapsule={timeCapsule}/>
+  </div>
 }
 
-function OpenView () {
-  return <div>open</div>
+function OpenView ({timeCapsule}) {
+  return <div>
+    <TimeCapsuleCard timeCapsule={timeCapsule}/>
+  </div>
 }
 
-function WrontStateView () {
-  return <div>wrong state</div>
+function WrongStateView () {
+  return <Alert color="danger">状态错误。</Alert>
 }
+
+const TimeCapsuleCard = compose(
+  withProps({
+    styles: {
+      content: css({
+        'margin': 0,
+        'whiteSpace': 'pre-wrap'
+      })
+    }
+  })
+)(function TimeCapsuleCard ({timeCapsule, styles}) {
+  return <Card>
+    <CardBlock>
+      <CardText>胶囊ID：{timeCapsule._id}</CardText>
+      <CardText>开启时间：{moment(timeCapsule.openTime).format('YYYY-MM-DD HH:mm:ss')}</CardText>
+      {
+        timeCapsule.content && <div>
+          <hr/>
+          <p {...styles.content}>{timeCapsule.content}</p>
+        </div>
+      }
+    </CardBlock>
+  </Card>
+})

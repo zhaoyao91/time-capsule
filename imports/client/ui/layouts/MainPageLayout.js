17f16c7e91f7e9279bf18db6652511a4b6e38111
zhaoyao91 <zhaoyao91@163.com>
2017-05-25 22:15:37 +0800

    Remove fix top layout

--- imports/client/ui/layouts/MainPageLayout.js
+++ imports/client/ui/layouts/MainPageLayout.js
@@ -8,35 +8,17 @@ import Container from '../views/Container'
 export default compose(
   withProps({
     styles: {
-      layout: css({
-        'display': 'flex',
-        'flexDirection': 'column',
-        'width': '100vw',
-        'height': '100vh',
-      }),
-      top: css({
-        'flexShrink': 0
-      }),
-      main: css({
-        'height': '100%',
-        'overflowY': 'auto',
-      }),
       mainContainer: css({
-        'minHeight': '100%',
         'paddingTop': '1rem',
         'paddingBottom': '1rem',
       })
     }
   })
 )(function MainPageLayout ({styles, children}) {
-  return <div {...styles.layout}>
-    <div {...styles.top}>
-      <MainNavbar/>
-    </div>
-    <div {...styles.main}>
-      <Container {...styles.mainContainer}>
-        {children}
-      </Container>
-    </div>
+  return <div>
+    <MainNavbar/>
+    <Container {...styles.mainContainer}>
+      {children}
+    </Container>
   </div>
 })

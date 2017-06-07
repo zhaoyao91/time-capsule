e39a5b657c88202f7aeee8970927e315995dfcdd
zhaoyao91 <zhaoyao91@163.com>
2017-06-07 09:39:45 +0800

    alert if failed to upload image

--- imports/client/ui/views/TimeCapsuleContentEditor.js
+++ imports/client/ui/views/TimeCapsuleContentEditor.js
@@ -1,38 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Editor } from 'react-draft-wysiwyg'
-import { compose, withProps } from 'recompose'
+import { withProps } from 'recompose'
 import { Random } from 'meteor/random'
 
+import withAlert from '../../hocs/with_alert'
 import FileService from '../../services/file'
 
-export default compose(
-  withProps(() => ({
-    toolbar: {
-      options: ['blockType', 'inline', 'list', 'colorPicker', 'link', 'embedded', 'image'],
-      inline: {
-        options: ['bold', 'italic', 'underline', 'strikethrough'],
-      },
-      blockType: {
-        options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
-      },
-      image: {
-        async uploadCallback(file) {
-          try {
-            const key = await FileService.uploadTimeCapsuleContentImage(file)
-            const url = FileService.getUrl(key)
-            return {data: {link: url}}
-          }
-          catch (err) {
-            console.error(err)
-            throw err
-          }
+@withAlert('alert')
+@withProps(({alert}) => ({
+  toolbar: {
+    options: ['blockType', 'inline', 'list', 'colorPicker', 'link', 'embedded', 'image'],
+    inline: {
+      options: ['bold', 'italic', 'underline', 'strikethrough'],
+    },
+    blockType: {
+      options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
+    },
+    image: {
+      async uploadCallback(file) {
+        try {
+          const key = await FileService.uploadTimeCapsuleContentImage(file)
+          const url = FileService.getUrl(key)
+          return {data: {link: url}}
+        }
+        catch (err) {
+          console.error(err)
+          alert.error('图片上传失败')
+          throw err
         }
       }
-    },
-    localization: {
-      locale: 'zh'
     }
-  })),
-)(function TimeCapsuleContentEditor ({styles, toolbar, localization, ...otherProps}) {
-  return <Editor toolbar={toolbar} localization={localization} {...otherProps}/>
-})
+  },
+  localization: {
+    locale: 'zh'
+  }
+}))
+export default class TimeCapsuleContentEditor extends Component {
+  render() {
+    const {toolbar, localization, ...otherProps} = this.props
+    return <Editor toolbar={toolbar} localization={localization} {...otherProps}/>
+  }
+}

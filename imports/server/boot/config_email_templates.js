857777087e692a76dc972ce79b8b3e6727dfed80
zhaoyao91 <zhaoyao91@163.com>
2017-06-14 00:13:31 +0800

    Get email from from settings

--- imports/server/boot/config_email_templates.js
+++ imports/server/boot/config_email_templates.js
@@ -1,11 +1,14 @@
 import { Accounts } from 'meteor/accounts-base'
+import { prop } from 'lodash/fp'
+
+import settings from '../settings'
 
 export default function () {
   Accounts.emailTemplates.siteName = '时光胶囊'
-  // Accounts.emailTemplates.from  =
+  Accounts.emailTemplates.from = prop('emailTemplates.from', settings)
 
   Accounts.emailTemplates.resetPassword = {
-    subject: () => '重置密码',
+    subject: () => '时光胶囊：重置密码',
     html: (user, url) => `
       <p>您好，亲爱的时光胶囊用户：</p>
       <p>请点击以下链接或复制链接到浏览器打开以重置密码（如非本人操作请忽略）</p>
--- imports/server/settings.js
+++ imports/server/settings.js
@@ -8,5 +8,8 @@ export default {
     bucket: need(prop('settings.public.nos.bucket', Meteor)),
     accessKey: need(prop('settings.nos.accessKey', Meteor)),
     accessSecret: need(prop('settings.nos.accessSecret', Meteor)),
-  }
+  },
+  emailTemplates: {
+    from: need(prop('settings.emailTemplates.from', Meteor)),
+  },
 }

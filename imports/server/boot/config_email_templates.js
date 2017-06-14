a8187020ac24cfb293fd72117cc38597254881d5
zhaoyao91 <zhaoyao91@163.com>
2017-06-14 10:34:53 +0800

    Change email subject

--- imports/server/boot/config_email_templates.js
+++ imports/server/boot/config_email_templates.js
@@ -8,7 +8,7 @@ export default function () {
   Accounts.emailTemplates.from = prop('emailTemplates.from', settings)
 
   Accounts.emailTemplates.resetPassword = {
-    subject: () => '时光胶囊：重置密码',
+    subject: () => '重置密码',
     html: (user, url) => `
       <p>您好，亲爱的时光胶囊用户：</p>
       <p>请点击以下链接或复制链接到浏览器打开以重置密码（如非本人操作请忽略）</p>
